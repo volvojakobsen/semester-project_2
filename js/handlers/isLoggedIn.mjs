0884e3d68e5adc262b469b9947c88c9eb767aaf8
volvojakobsen <91479420+volvojakobsen@users.noreply.github.com>
2022-12-02 17:03:45 +0100

    login href

--- js/handlers/isLoggedIn.mjs
+++ js/handlers/isLoggedIn.mjs
@@ -5,5 +5,8 @@ const loginButton = document.querySelector("#nav_login");
 export function isUserLoggedIn() {
     if (storage.load("token")) {
         loginButton.innerHTML = "LOGOUT";
+        loginButton.addEventListener("click", (e) => {
+            localStorage.clear();
+        });
     }
 }
