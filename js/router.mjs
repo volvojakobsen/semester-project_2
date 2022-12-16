61589f89b93730586d9f0bf05d01fe4f9e0637b7
volvojakobsen <91479420+volvojakobsen@users.noreply.github.com>
2022-12-16 10:43:12 +0100

    fixing bugs for git hub pages

--- js/router.mjs
+++ js/router.mjs
@@ -15,39 +15,42 @@ export default function router() {
             listeners.isUserLoggedIn()
 
             return;
+        case `/register.html`:
         case `/semester-project_2/register.html`:
             listeners.setRegisterFormListener()
             listeners.isUserLoggedIn()
             return;
+        case `/login.html`:
         case `/semester-project_2/login.html`:
             listeners.setLoginFormListener()
             listeners.isUserLoggedIn()
             return;
         case `/semester-project_2/createItem.html`:
+        case `/createItem.html`:
             listeners.setCreateItemFormListener()
             listeners.isUserLoggedIn()
             return;
         case `/semester-project_2/updateItem.html`:
+        case `/updateItem.html`:
             listeners.setUpdateItemListener()
             listeners.isUserLoggedIn()
             return;
         case `/semester-project_2/removeItem.html`:
+        case `/removeItem.html`:
             listeners.setDeleteItemListener()
             listeners.isUserLoggedIn()
             return;
         case `/semester-project_2/singleItem.html`:
+        case `/singleItem.html`:
             listeners.displayItemListener()
             listeners.bidOnItemFormListener()
             listeners.isUserLoggedIn()
             return;
         case `/semester-project_2/profile.html`:
+        case `/profile.html`:
             listeners.displayProfileListener()
             listeners.isUserLoggedIn()
             profileListeners.updateAvatarFormListener()
-            return;
-        case `/semester-project_2/index.html`:
-            listeners.displayItems()
-            listeners.isUserLoggedIn()
             return;
     }
 
